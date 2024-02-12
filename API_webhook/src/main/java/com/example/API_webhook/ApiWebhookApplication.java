539985510ca1ff60cce2d2b434ecc0b7b9d26bea
Thicasella <thiago.mcasella@gmail.com>
2024-02-12 02:32:20 -0300

    primeiro commit

---
+++ API_webhook/src/main/java/com/example/API_webhook/ApiWebhookApplication.java
@@ -0,0 +1,13 @@
+package com.example.API_webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWebhookApplication.class, args);
+	}
+
+}
---
+++ API_webhook/src/test/java/com/example/API_webhook/ApiWebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
